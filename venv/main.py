b6f0c1d72e95faa260db7becdb64cd7d02c1f569
TabeSebastian <s.althabe@gmail.com>
2023-10-23 19:24:53 -0300

    23/10

--- venv/main.py
+++ venv/main.py
@@ -1,120 +1,132 @@
-
+from fastapi import FastAPI
 import pandas as pd
+import json
 
-# abir archivo 
-archivo_json = 'E:/Mis documentos/henry/proyecto_final_1_reaload/venv/Include/output_steam_games.json'
+app = FastAPI()
 
-# Carga el archivo JSON en un DataFrame
-df_juegos = pd.read_json(archivo_json, lines=True)
+# Carga el DataFrame con los datos fuera de la función
+
+df_PlayTimeGenre = pd.read_csv('E:/Mis documentos/henry/proyecto_final_1_reaload/venv/Include/def PlayTimeGenre_final_listo.csv')
+
+ruta_archivo_json = 'E:/Mis documentos/henry/proyecto_final_1_reaload/venv/Include/def UserForGenre.json'
+with open(ruta_archivo_json, 'r') as archivo_json:
+    datosUserForGenre = json.load(archivo_json)
+
+df_UsersRecommend = pd.read_csv('E:/Mis documentos/henry/proyecto_final_1_reaload/venv/Include/def UsersRecommend.csv')
+
+df_UsersNotRecommend = pd.read_csv('E:/Mis documentos/henry/proyecto_final_1_reaload/venv/Include/bottom_3_por_anio.csv')
+
+df_sentiment_analysis = pd.read_csv('E:/Mis documentos/henry/proyecto_final_1_reaload/venv/Include/def sentiment_analysis.csv')
 
 
-def clean_df_juegos(df_juegos):
+#instanciamos las api y sus rutas
+
+@app.get('/')
+def main():
+    return {"mensaje": "BIENVENIDOS"}
+
+@app.get('/PlayTimeGenre/{genero}')
+def PlayTimeGenre(genero: str):
+
+    # Filtrar el DataFrame por género específico
+    df_genero = df_PlayTimeGenre[df_PlayTimeGenre['Genre'] == genero]
     
-    # Drop rows with missing data across all columns
-    df_juegos = df_juegos.dropna()
-    # Drop column: 'url'
-    df_juegos = df_juegos.drop(columns=['url'])
-    # Drop column: 'price'
-    df_juegos = df_juegos.drop(columns=['price'])
-    # Drop column: 'early_access'
-    df_juegos = df_juegos.drop(columns=['early_access'])
-    # Drop column: 'reviews_url'
-    df_juegos = df_juegos.drop(columns=['reviews_url'])
-    # Sort by column: 'release_date' (ascending)
-    df_juegos = df_juegos.sort_values(['release_date'])
-    # Sort by column: 'release_date' (descending)
-    df_juegos = df_juegos.sort_values(['release_date'], ascending=[False])
-    # Replace all instances of "Sep 2014" with "2014-09-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Sep 2014", "2014-09-01", case=False, regex=False)
-    # Replace all instances of "Sep 2009" with "2009-09-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Sep 2009", "2009-09-01", case=False, regex=False)
-    # Replace all instances of "SOON™" with "2025-01-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("SOON™", "2025-01-01", case=False, regex=False)
-    # Replace all instances of "SOON" with "2025-01-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("SOON", "2025-01-01", case=False, regex=False)
-    # Replace all instances of "Oct 2016" with "2016-10-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Oct 2016", "2016-10-01", case=False, regex=False)
-    # Replace all instances of "Oct 2010" with "2010-10-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Oct 2010", "2010-10-01", case=False, regex=False)
-    # Replace all instances of "Oct 2009" with "2009-10-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Oct 2009", "2009-10-01", case=False, regex=False)
-    # Replace all instances of "Nov 2016" with "2016-11-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Nov 2016", "2016-11-01", case=False, regex=False)
-    # Replace all instances of "Nov 2014" with "2014-11-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Nov 2014", "2014-11-01", case=False, regex=False)
-    # Replace all instances of "May 2015" with "2015-05-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("May 2015", "2015-05-01", case=False, regex=False)
-    # Replace all instances of "May 2014" with "2014-05-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("May 2014", "2014-05-01", case=False, regex=False)
-    # Replace all instances of "Mar 2010" with "2010-03-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Mar 2010", "2010-03-01", case=False, regex=False)
-    # Replace all instances of "Jun 2016" with "2016-06-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Jun 2016", "2016-06-01", case=False, regex=False)
-    # Replace all instances of "Jun 2015" with "2015-06-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Jun 2015", "2015-06-01", case=False, regex=False)
-    # Replace all instances of "Jun 2009" with "2009-06-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Jun 2009", "2009-06-01", case=False, regex=False)
-    # Replace all instances of "Jul 2017" with "2017-07-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Jul 2017", "2017-07-01", case=False, regex=False)
-    # Replace all instances of "Jul 2016" with "2016-07-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Jul 2016", "2016-07-01", case=False, regex=False)
-    # Replace all instances of "Jul 2014" with "2014-07-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Jul 2014", "2014-07-01", case=False, regex=False)
-    # Replace all instances of "Jul 2010" with "2010-07-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Jul 2010", "2010-07-01", case=False, regex=False)
-    # Replace all instances of "Jan 2017" with "2017-01-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Jan 2017", "2017-01-01", case=False, regex=False)
-    # Replace all instances of "Jan 2015" with "2015-01-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Jan 2015", "2015-01-01", case=False, regex=False)
-    # Replace all instances of "Jan 2010" with "2010-01-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Jan 2010", "2010-01-01", case=False, regex=False)
-    # Replace all instances of "Feb 2013" with "2013-02-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Feb 2013", "2013-02-01", case=False, regex=False)
-    # Replace all instances of "Feb 2011" with "2011-02-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Feb 2011", "2011-02-01", case=False, regex=False)
-    # Replace all instances of "Dec 2012" with "2012-12-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Dec 2012", "2012-12-01", case=False, regex=False)
-    # Replace all instances of "Aug 2015" with "2015-08-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Aug 2015", "2015-08-01", case=False, regex=False)
-    # Replace all instances of "Aug 2014" with "2014-08-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Aug 2014", "2014-08-01", case=False, regex=False)
-    # Replace all instances of "Apr 2017" with "2017-04-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Apr 2017", "2017-04-01", case=False, regex=False)
-    # Replace all instances of "Apr 2016" with "2016-04-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Apr 2016", "2016-04-01", case=False, regex=False)
-    # Replace all instances of "Jun 2009" with "2009-06-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Jun 2009", "2009-06-01", case=False, regex=False)
-    # Replace all instances of "Jun 2009" with "2009-06-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Jun 2009", "2009-06-01", case=False, regex=False)
-    # Replace all instances of "Oct 2010" with "2010-10-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Oct 2010", "2010-10-01", case=False, regex=False)
-    # Replace all instances of "Feb 2011" with "2011-02-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Feb 2011", "2011-02-01", case=False, regex=False)
-    # Replace all instances of "Sep 2014" with "201-09-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Sep 2014", "2014-09-01", case=False, regex=False)
-    # Replace all instances of "Apr 2016" with "2016-04-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Apr 2016", "2016-04-01", case=False, regex=False)
-    # Replace all instances of "Jul 2017" with "2017-07-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("Jul 2017", "2017-07-01", case=False, regex=False)
-    # Replace all instances of "SOON" with "2025-01-01" in column: 'release_date'
-    df_juegos['release_date'] = df_juegos['release_date'].str.replace("SOON", "2025-01-01", case=False, regex=False)
-    # Filter rows based on column: 'release_date'
-    df_juegos = df_juegos[~(df_juegos['release_date'] == "2018")]
-    # Split text using string '-' in column: 'release_date'
-    loc_0 = df_juegos.columns.get_loc('release_date')
-    df_juegos_split = df_juegos['release_date'].str.split(pat='-', expand=True).add_prefix('release_date_')
-    df_juegos = pd.concat([df_juegos.iloc[:, :loc_0], df_juegos_split, df_juegos.iloc[:, loc_0:]], axis=1)
-    df_juegos = df_juegos.drop(columns=['release_date'])
-    # Rename column 'release_date_0' to 'release_date_anio'
-    df_juegos = df_juegos.rename(columns={'release_date_0': 'release_date_anio'})
-    # Rename column 'release_date_1' to 'release_date_mes'
-    df_juegos = df_juegos.rename(columns={'release_date_1': 'release_date_mes'})
-    # Rename column 'release_date_2' to 'release_date_dia'
-    df_juegos = df_juegos.rename(columns={'release_date_2': 'release_date_dia'})
+    if df_genero.empty:
+        # Obtener la lista de géneros disponibles en el DataFrame
+        generos_disponibles = df_PlayTimeGenre['Genre'].unique().tolist()
+        return {"mensaje": "Género inexistente, por favor ingrese un género válido", "generos_disponibles": generos_disponibles}
+                
+    # Encontrar el año con las horas máximas para el género
+    max_year = df_genero[df_genero['Total Hours'] == df_genero['Total Hours'].max()]['Year'].values[0]
 
-    # reset index
-    df_juegos = df_juegos.reset_index(drop=True)
-    
-    return df_juegos
+    return {"Año de lanzamiento con más horas jugadas para " + genero: int(max_year)}
 
-df_juegos= clean_df_juegos(df_juegos)
+
+
+@app.get('/UserForGenre/{genero}')
+def UserForGenre(genero: str):
+    if 'Usuario con más horas jugadas para ' + genero not in datosUserForGenre[0]:
+        generos_disponibles = list(datosUserForGenre[0].keys())
+        return {"mensaje": "Género no encontrado en los datos", "generos_disponibles": generos_disponibles}
+
+    # Busca el usuario con más horas jugadas para el género dado
+    usuario_con_mas_horas = datosUserForGenre[0]['Usuario con más horas jugadas para ' + genero]
+
+    # Obtiene la lista de acumulación de horas jugadas por año
+    acumulacion_horas_por_anio = datosUserForGenre[0]['Horas jugadas']
+
+    return {
+        "Usuario con más horas jugadas para " + genero: usuario_con_mas_horas,
+        "Acumulación de horas jugadas por año": acumulacion_horas_por_anio
+    }
+
+
+@app.get('/UsersRecommend/{anio}')
+def UsersRecommend(anio: int):
+    # Filtra el DataFrame para el año especificado
+    df_filtrado = df_UsersRecommend[df_UsersRecommend['Unnamed: 0'] == anio]
+
+    if df_filtrado.empty:
+        # Si el filtrado está vacío, devuelve todos los datos de la columna 'Unnamed: 0'
+        datos_anios = df_UsersRecommend['Unnamed: 0'].tolist()
+        return {"Todos los anios disponibles son ": datos_anios}
+
+    # Si se encontraron datos para el año especificado, selecciona los juegos recomendados
+    juegos_recomendados = df_filtrado[['Puesto 1', 'Puesto 2', 'Puesto 3']].iloc[0]
+
+    # Elimina valores NaN si los hubiera
+    juegos_recomendados = juegos_recomendados.dropna()
+
+    if juegos_recomendados.empty:
+        return {"mensaje": "No se encontraron juegos recomendados para el año especificado"}
+
+    # Convierte la serie de juegos recomendados en una lista
+    top_3_juegos = juegos_recomendados.tolist()
+
+    return {"Top 3 juegos recomendados para " + str(anio): top_3_juegos}
+
+@app.get('/UsersNotRecommend/{anio}')
+def UsersRecommend(anio: int):
+    # Filtra el DataFrame para el año especificado
+    df_filtrado = df_UsersNotRecommend[df_UsersNotRecommend['Unnamed: 0'] == anio]
+
+    if df_filtrado.empty:
+        # Si el filtrado está vacío, devuelve todos los datos de la columna 'Unnamed: 0'
+        datos_anios = df_UsersNotRecommend['Unnamed: 0'].tolist()
+        return {"Todos los anios disponibles son ": datos_anios}
+
+    # Si se encontraron datos para el año especificado, selecciona los juegos recomendados
+    juegos_recomendados = df_filtrado[['Puesto 1', 'Puesto 2', 'Puesto 3']].iloc[0]
+
+    # Elimina valores NaN si los hubiera
+    juegos_recomendados = juegos_recomendados.dropna()
+
+
+    # Convierte la serie de juegos recomendados en una lista
+    top_3_juegos = juegos_recomendados.tolist()
+
+    return {"Top 3 juegos recomendados para " + str(anio): top_3_juegos}
+
+@app.get('/SentimentAnalysis/{anio}')
+def sentiment_analysis(anio: int):
+    # Filtra el DataFrame para el año especificado
+    df_filtrado = df_sentiment_analysis[df_sentiment_analysis['Unnamed: 0'] == anio]
+
+    if df_filtrado.empty:
+        # Si el filtrado está vacío, devuelve todos los datos de la columna 'Unnamed: 0'
+        datos_anios = df_sentiment_analysis['Unnamed: 0'].tolist()
+        return {"Todos los años disponibles son": datos_anios}
+
+    # Si se encontraron datos para el año especificado, selecciona el análisis de sentimiento
+    analisis_sentimiento = df_filtrado[['Positive', 'Negative']].iloc[0]
+
+    # Elimina valores NaN si los hubiera
+    analisis_sentimiento = analisis_sentimiento.dropna()
+
+    if analisis_sentimiento.empty:
+        return {"mensaje": "No se encontró análisis de sentimiento para el año especificado"}
+
+    # Convierte la serie de análisis de sentimiento en un diccionario
+    analisis_dict = analisis_sentimiento.to_dict()
+
+    return {"Análisis de sentimiento para el año " + str(anio): analisis_dict}
